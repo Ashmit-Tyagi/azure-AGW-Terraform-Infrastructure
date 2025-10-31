497f5e7c49d86fe38e388fee1c21a8e447ead2a2
Ashmit Tyagi <151730397+Ashmit-Tyagi@users.noreply.github.com>
2025-10-31 23:16:38 +0530

    Update backend.tf

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "dev-rg"
-    storage_account_name = ""
-    container_name       = ""
+    storage_account_name = "devopskawork7"
+    container_name       = "spooky-dev"
     key                  = "vnet-compute/dev.terraform.tfstate"
     use_azuread_auth     = true
   }
